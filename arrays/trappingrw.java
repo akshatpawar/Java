98744ec6572a0be8649dde9271e676246da530b3
akshat <akshatpawar1@gmail.com>
2024-03-16 16:02:10 +0530

    LC 42

---
+++ arrays/trappingrw.java
@@ -0,0 +1,33 @@
+package arrays;
+
+public class trappingrw {
+    public static int TreappingRainwater(int height[]){
+        int n = height.length;
+        int leftMax[] = new int[n];
+        int rightMax[] = new int[n];
+
+        leftMax[0] = height[0];
+        for (int i=1; i<n; i++) {
+            leftMax[i]=Math.max(leftMax[i-1], height[i]);
+        }
+
+        rightMax[n-1] = height[n-1];
+        for (int i=n-2; i>=0; i--) {
+            rightMax[i]=Math.max(rightMax[i+1], height[i]);
+        }
+
+        int trappedWater=0;
+
+        for (int i=0; i<n; i++) {
+            int waterLevel= Math.min(leftMax[i], rightMax[i]);
+            trappedWater+=waterLevel - height[i];
+        }
+
+        return trappedWater;
+    }
+
+    public static void main(String args[]) {
+        int height[] = {4,2,0,6,3,2,5,0,2,4};
+        System.out.println(TreappingRainwater(height));
+    }
+}
